66064e3745b16695bc0c45ca32f2df5aa92d12ed
Edward Parkinson <e.j.parkinson@soton.ac.uk>
2018-04-16 12:51:52 +0100

    Adding functioning edge code using a virtual topology as well as adding various examples of using MPI_Cart_shift.

---
+++ codes/edges_dynamic_MPI_cart/source/find_edges.h
@@ -0,0 +1,19 @@
+void pgmsize (char *filename, int *nx, int *ny);
+void pgmread (char *filename, void *vx, int nx, int ny);
+void pgmwrite(char *filename, void *vx, int nx, int ny);
+
+void *arralloc(size_t size, int ndim, ...);
+
+#define DEFAULT_COMM MPI_COMM_WORLD
+#define DEFAULT_TAG 0
+#define MASTER_PROCESS 0
+
+#define NDIMS 1
+#define XDIR 0
+#define YDIR 1
+
+#define N_NBRS 4
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
